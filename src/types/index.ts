e07254df80c3070e6f5d4ded372bb0cfa9e5b0f4
sowmik-sec <sowmik.sec@gmail.com>
2025-05-05 19:51:10 +0600

    Caching and revalidating the latest blogs on the homepage

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export default interface Blog {
+  id: string;
+  title: string;
+  description: string;
+  publish_date: string;
+  author_name: string;
+  blog_image: string;
+  total_likes: string;
+}
