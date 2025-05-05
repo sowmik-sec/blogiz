26ba0166aba90cf89b6813ee2fa3fa30964df396
sowmik-sec <sowmik.sec@gmail.com>
2025-05-05 20:17:40 +0600

    Retrieve dynamic data with an ID for the detail page using SSR

---
+++ src/app/blogs/[blogId]/page.tsx
@@ -0,0 +1,21 @@
+import BlogDetails from "@/components/ui/BlogDetails";
+
+interface BlogId {
+  params: {
+    blogId: string;
+  };
+}
+
+const BlogDetailPage = async ({ params }: BlogId) => {
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
+  const blog = await res.json();
+  return (
+    <div className="my-10">
+      <BlogDetails blog={blog} />
+    </div>
+  );
+};
+
+export default BlogDetailPage;
